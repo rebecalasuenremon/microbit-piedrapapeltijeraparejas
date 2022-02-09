178650add74bd76a4ecacb32a01dff93db8d847d
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-02-09 08:50:07 +0100

    hf

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-
+input.onGesture(Gesture.Shake, function () {
+    Jugada = randint(1, 3)
+    radio.sendNumber(randint(1, 3))
+    if (Jugada == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Jugada == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+let Jugada = 0
+radio.setGroup(7)
+basic.forever(function () {
+	
+})
